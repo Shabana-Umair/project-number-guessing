b0aa2e94fdb0d060d05756b885ed898c1d59a8f2
Shabana-Umair <shabanaumair1236@gmail.com>
2024-04-06 01:04:20 -0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
 // 1)computer will generate a random number
 // 2)user input for guessing number
 // 3)compare user input with computer generated number and show result
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 console.log(randomNumber);
-const answers = await inquirer.prompt([
+const answers = await inquirer_1.default.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
